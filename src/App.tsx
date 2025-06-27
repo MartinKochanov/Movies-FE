4b2d0234479d27044c75f94c07099e12dee8b283
MartinKochanov <114015998+MartinKochanov@users.noreply.github.com>
2025-06-27 10:59:49 +0300

    fix: change theme of the toast notification based on the theme selected

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { AuthProvider } from "./context/AuthContext";
 import { CustomThemeProvider } from "./context/ThemeContext";
+import ToastNotification from "./features/shared/toast/ToastNotification";
 import { appRouter } from "./routes/Router";
 
 function App() {
   return (
     <AuthProvider>
       <CustomThemeProvider>
+        <ToastNotification />
         <RouterProvider router={appRouter} />;
       </CustomThemeProvider>
     </AuthProvider>
--- src/features/shared/toast/ToastNotification.tsx
+++ src/features/shared/toast/ToastNotification.tsx
@@ -1,7 +1,12 @@
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import { useTheme } from "../../../context/ThemeContext";
+import { Theme } from "../../../types/Shared";
+
 const ToastNotification = () => {
+  const { theme } = useTheme();
+
   return (
     <ToastContainer
       position="top-right"
@@ -13,7 +18,7 @@ const ToastNotification = () => {
       pauseOnFocusLoss
       draggable
       pauseOnHover
-      theme="dark"
+      theme={theme === Theme.DARK ? "dark" : "light"}
     />
   );
 };
